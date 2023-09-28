d31d78d63c10963115103ec9e13575f2812b4a53
Taylor McGregor <mcgregor.taylor44@gmail.com>
2023-09-28 09:03:38 -0500

    practiced with api

--- APIsAndJSON/OpenWeatherMapAPI.cs
+++ APIsAndJSON/OpenWeatherMapAPI.cs
@@ -1,12 +1,30 @@
-﻿using System;
+﻿using Newtonsoft.Json.Linq;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using static System.Net.WebRequestMethods;
 
 namespace APIsAndJSON
 {
     internal class OpenWeatherMapAPI
     {
+        //api key is 6d056eab11773a5e998752c9c36f95ba
+        //https://api.openweathermap.org/data/2.5/weather?lat={lat}&lon={lon}&appid={API key}
+        // The latitude of Trussville is 33.619N. The longitude is -86.608W.
+        public static void TrussvilleWeather()
+        {
+            HttpClient client = new HttpClient();
+            var apiKey = "6d056eab11773a5e998752c9c36f95ba";
+            var cityName = "Trussville";
+            var weatherURL = $"https://api.openweathermap.org/data/2.5/weather?q={cityName}&appid={apiKey}&units=imperial"; ;
+            var weatherResponse = client.GetStringAsync(weatherURL).Result;
+            var weatherLocal = JObject.Parse(weatherResponse);      
+            Console.WriteLine($"The weather in {cityName} is {weatherLocal["main"]["temp"]} degrees Fahrenheit");
+
+
+
+        }
     }
 }
--- APIsAndJSON/RonVSKanyeAPI.cs
+++ APIsAndJSON/RonVSKanyeAPI.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Newtonsoft.Json.Linq;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,5 +9,24 @@ namespace APIsAndJSON
 {
     internal class RonVSKanyeAPI
     {
-    }
+        public static void Convo()
+        {
+             HttpClient client = new HttpClient();
+                      
+            for (int i = 0; i < 6; i++)
+            {        
+                string ronURL = "https://ron-swanson-quotes.herokuapp.com/v2/quotes";
+                var ronResponse = client.GetStringAsync(ronURL).Result;
+                var ronQuote = JArray.Parse(ronResponse);
+
+                string kanyeURL = "https://api.kanye.rest/";
+                var kanyeResponse = client.GetStringAsync(kanyeURL).Result;
+                var kanyeQuote = JObject.Parse(kanyeResponse);
+                Console.WriteLine($"Ron:{ronQuote[0]}");
+                Console.WriteLine();
+                Console.WriteLine($"Kanye:{kanyeQuote["quote"]}.");
+                Console.WriteLine();
+            }
+        }
+    }  
 }
